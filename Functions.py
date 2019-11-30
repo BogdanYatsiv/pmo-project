5e3c958953938ae0eca1d854e336682ab0d6a124
BogdanYatsiv <bogdanyatsiv@gmail.com>
2019-11-30 20:18:55 +0200

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -1,28 +1,27 @@
 import numpy as np
 
-def operatorA_action(V0:np.poly1d, number:int)->list:
-    """ Obtaining successive iterations Vm using the integral operator kernel """
-    p = V0 # задаємо початкове наближення
+def operatorA_action(p:np.poly1d)->np.poly1d:
+    """ Integral operator action A """
     t = np.array([1,0]) #t
     t1 = np.array([-1,1]) #1-t
-    m = 1
-    list_V = [] # список поліномів Vm
-    list_V.append(V0)
 
-    while m < number:
-        first_int = np.polyint(np.polymul(t,p)) # інтегруємо першу частину
-        first_addition = np.polymul(t1, first_int) # знаходимо перший доданок
+    first_int = np.polyint(np.polymul(t,p)) # інтегруємо першу частину
+    first_addition = np.polymul(t1, first_int) # знаходимо перший доданок
 
-        second_int = np.polyint(np.polymul(t1,p)) # знаходимо первісну
-        second_addition = np.polymul(t, np.polysub(np.polyval(second_int,1), second_int)) # підставляємо межі інтегрування та знаходимо другий доданок
+    second_int = np.polyint(np.polymul(t1,p)) # знаходимо первісну
+    second_addition = np.polymul(t, np.polysub(np.polyval(second_int,1), second_int)) # підставляємо межі інтегрування та знаходимо другий доданок
 
-        Vm = np.polyadd(first_addition, second_addition) #сумуємо два доданки
-        p = Vm # попередній поліном
+    Vm = np.polyadd(first_addition, second_addition) #сумуємо два доданки
 
-        list_V.append(Vm)
-        m += 1
+    return Vm
 
-    return list_V
+def sequence_V(V0:np.poly1d, m:int)->list:
+    """ The function generates the sequence Vm """
+    sequence = []
+    sequence.append(V0) # додаємо початкове наближення
+    for i in range (m):
+        sequence.append(operatorA_action(sequence[i])) # додаємо поліном в послідовність 
+    return sequence
 
 def scalar_product(a_poly:np.poly1d, b_poly:np.poly1d)->float:
     """Find scalar product of two polynomials"""
